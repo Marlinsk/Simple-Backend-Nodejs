f3084c04532ab7701f6c8e6db85d058bfffac501
Marlon <Marlonnlmx@outlook.com>
2022-05-15 19:07:03 -0300

    CRUD implemented and tested successfully!

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,22 @@ app.get("/", async (request, response) => {
   return response.send("Hello World!");
 });
 
+app.get("/feed/", async (request, response) => {
+  const users = await prisma.user.findMany();
+  response.json({ users });
+});
+
+app.get("/:id/profile", async (request, response) => {
+  const { id } = request.params;
+  const user = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  response.json({ user });
+});
+
 app.post("/create-user", async (request, response) => {
   const { completename, username, email, password } = request.body;
   const user = await prisma.user.create({
@@ -22,9 +38,41 @@ app.post("/create-user", async (request, response) => {
       password,
     },
   });
+
   return response.json({ user });
 });
 
+app.put("/:id/edit-user", async (request, response) => {
+  const { id } = request.params;
+  const { completename, username, email, password } = request.body;
+
+  const user = await prisma.user.update({
+    where: { id },
+    data: {
+      completename,
+      username,
+      email,
+      password,
+    },
+  });
+
+  response.json({ user });
+});
+
+app.delete("/:id/delete", async (request, response) => {
+  const { id } = request.params;
+  const user = await prisma.user.delete({
+    where: {
+      id,
+    },
+  });
+  response.json({
+    user: {
+      message: "User successfully deleted from dataset",
+    },
+  });
+});
+
 app.listen(port, () => {
-  console.log(`Server running at localhost:${port}`);
+  console.log(`Server running in localhost:${port}`);
 });
