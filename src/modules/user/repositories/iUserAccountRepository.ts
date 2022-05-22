65d613298c28c13a11efb53fc654985cb615cfc0
Marlon <Marlonnlmx@outlook.com>
2022-05-22 20:54:50 -0300

    Implementation: iUserAccountRepository.ts

---
+++ src/modules/user/repositories/iUserAccountRepository.ts
@@ -0,0 +1,13 @@
+import { User } from "../../../domain/entity/User";
+import { ICreateUserAccountDTO } from "../dto/iCreateUserAccountDTO";
+
+export interface IUserAccountRepository {
+  create(data: ICreateUserAccountDTO): Promise<void>;
+  // save(user: User): Promise<User>;
+  // delete(id: string): Promise<void>;
+  // findAllUsers(): Promise<User>;
+  // findById(id: string): Promise<User | null>;
+  // findByCompleteName(completename: string): Promise<User | null>;
+  // findByUsername(username: string): Promise<User | null>;
+  // findByEmail(email: string): Promise<User | null>;
+}
