a39c448d67ecbdbd04e45c40b225cf98fa157108
Marlon <Marlonnlmx@outlook.com>
2022-05-23 20:27:32 -0300

    Implementation: User account update controller

---
+++ src/modules/user/infra/http/controllers/update-user-account-controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { UpdateUserAccountUseCase } from "../../../../../usecases/update-user-account-usecase";
+
+export class UpdateUserAccountController {
+  constructor(private updateUserAccountUseCase: UpdateUserAccountUseCase) {}
+
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const { completename, username, email, password } = request.body;
+
+    try {
+      await this.updateUserAccountUseCase.execute({
+        id,
+        completename,
+        username,
+        email,
+        password,
+      });
+      return response
+        .status(200)
+        .json({ message: "User successfully updated in in dataset" });
+    } catch (error) {
+      return response.status(400).json();
+    }
+  }
+}
