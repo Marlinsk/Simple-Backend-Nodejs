5d0dd6acdde7f3b5f4881e54612a5d11f7b9dd98
Marlon <Marlonnlmx@outlook.com>
2022-05-23 20:27:09 -0300

    Implementation: User account update service

---
+++ src/usecases/update-user-account-usecase.ts
@@ -0,0 +1,37 @@
+import { User } from "../domain/entity/User";
+import { IUserAccountRepository } from "../modules/user/repositories/iUserAccountRepository";
+
+interface IUpdateUserAccountUseCaseRequest {
+  id: string;
+  completename: string;
+  username: string;
+  email: string;
+  password: string;
+}
+
+export class UpdateUserAccountUseCase {
+  constructor(private userAccountRepository: IUserAccountRepository) {}
+
+  async execute({
+    id,
+    completename,
+    username,
+    email,
+    password,
+  }: IUpdateUserAccountUseCaseRequest): Promise<User> {
+    // const checkUserID = await this.userAccountRepository.findById(id);
+
+    // if (!checkUserID) {
+    // }
+
+    const updateUser = await this.userAccountRepository.update({
+      id,
+      completename,
+      username,
+      email,
+      password,
+    });
+
+    return updateUser;
+  }
+}
