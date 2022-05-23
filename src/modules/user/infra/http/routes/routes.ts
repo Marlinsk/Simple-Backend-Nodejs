8be24dbb5de96c733be399b826010d81f733f98e
Marlon <Marlonnlmx@outlook.com>
2022-05-23 20:52:23 -0300

    Feature: Implementing put to route

--- src/modules/user/infra/http/routes/routes.ts
+++ src/modules/user/infra/http/routes/routes.ts
@@ -1,20 +1,34 @@
 import express, { request, response, Router } from "express";
+
 import { CreateUserAccountUseCase } from "../../../../../usecases/create-user-account-usecase";
+import { UpdateUserAccountUseCase } from "../../../../../usecases/update-user-account-usecase";
+
 import { PrismaUserAccountRepository } from "../../../repositories/implementations/prisma-user-account-repository";
 import { CreateUserAccountController } from "../controllers/create-user-account-controller";
+import { UpdateUserAccountController } from "../controllers/update-user-account-controller";
 
 export const routes = express.Router();
 
 routes.post("/create-user", async (request, response) => {
   const prismaUserAccountRepository = new PrismaUserAccountRepository();
-
   const createUserAccountUseCase = new CreateUserAccountUseCase(
     prismaUserAccountRepository
   );
-
   const createUserAccountController = new CreateUserAccountController(
     createUserAccountUseCase
   );
 
   return await createUserAccountController.handle(request, response);
+});
+
+routes.put("/edit-user/:id", async (request, response) => {
+  const prismaUserAccountRepository = new PrismaUserAccountRepository();
+  const updateUserAccountUseCase = new UpdateUserAccountUseCase(
+    prismaUserAccountRepository
+  );
+  const updateUserAccountController = new UpdateUserAccountController(
+    updateUserAccountUseCase
+  );
+
+  return await updateUserAccountController.handle(request, response);
 });
