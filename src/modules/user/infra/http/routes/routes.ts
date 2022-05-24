5811ac10d68d5cb0e3ebad110ef660b4899d92db
Marlon <Marlonnlmx@outlook.com>
2022-05-24 10:54:32 -0300

    Feature: Implementing delete to route

--- src/modules/user/infra/http/routes/routes.ts
+++ src/modules/user/infra/http/routes/routes.ts
@@ -1,15 +1,17 @@
 import express, { request, response, Router } from "express";
 
-import { CreateUserAccountUseCase } from "../../../../../usecases/create-user-account-usecase";
-import { UpdateUserAccountUseCase } from "../../../../../usecases/update-user-account-usecase";
+import { CreateUserAccountUseCase } from "../../../usecases/create-user-account-usecase";
+import { DeleteUserAccountUseCase } from "../../../usecases/delete-user-account-usecase";
+import { UpdateUserAccountUseCase } from "../../../usecases/update-user-account-usecase";
 
 import { PrismaUserAccountRepository } from "../../../repositories/implementations/prisma-user-account-repository";
 import { CreateUserAccountController } from "../controllers/create-user-account-controller";
+import { DeleteUserAccountController } from "../controllers/delete-user-account-controller";
 import { UpdateUserAccountController } from "../controllers/update-user-account-controller";
 
 export const routes = express.Router();
 
-routes.post("/create-user", async (request, response) => {
+routes.post("/create-user/", async (request, response) => {
   const prismaUserAccountRepository = new PrismaUserAccountRepository();
   const createUserAccountUseCase = new CreateUserAccountUseCase(
     prismaUserAccountRepository
@@ -31,4 +33,16 @@ routes.put("/edit-user/:id", async (request, response) => {
   );
 
   return await updateUserAccountController.handle(request, response);
+});
+
+routes.delete("/delete/:id", async (request, response) => {
+  const prismaUserAccountRepository = new PrismaUserAccountRepository();
+  const deleteUserAccountUseCase = new DeleteUserAccountUseCase(
+    prismaUserAccountRepository
+  );
+  const deleteUserAccountController = new DeleteUserAccountController(
+    deleteUserAccountUseCase
+  );
+
+  return deleteUserAccountController.handle(request, response);
 });
