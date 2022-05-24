699280686db6b84b337cda88fc33d6bbbfb826d6
Marlon <Marlonnlmx@outlook.com>
2022-05-24 10:53:54 -0300

    Implementation: User account delete controller

---
+++ src/modules/user/infra/http/controllers/delete-user-account-controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { DeleteUserAccountUseCase } from "../../../usecases/delete-user-account-usecase";
+
+export class DeleteUserAccountController {
+  constructor(private deleteUserAccountUseCase: DeleteUserAccountUseCase) {}
+
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    try {
+      await this.deleteUserAccountUseCase.execute(id);
+      return response.status(200).json({
+        message: "User successfully deleted from dataset",
+      });
+    } catch (error) {
+      return response.status(400).json();
+    }
+  }
+}
