b205dc987a7a47c03bc582a14eefdd5bb5997c57
Marlon <Marlonnlmx@outlook.com>
2022-05-25 12:05:51 -0300

    Implementation: Controller class

---
+++ src/infra/http/controllers/find-by-username-controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { FindByUsernameUseCase } from "../../../usecases/find-by-username-usecase";
+
+export class FindByUsernameController {
+  constructor(private findByUsernameUseCase: FindByUsernameUseCase) {}
+
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { username } = request.params;
+    try {
+      const user = await this.findByUsernameUseCase.execute(username);
+      return response
+        .status(200)
+        .json({ messsage: "Profile found successfully!", profile: user });
+    } catch (error) {
+      return response.status(400).json();
+    }
+  }
+}
