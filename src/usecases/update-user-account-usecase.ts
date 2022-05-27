4bf76bea0385ad6e6e824376e6dc69aa32e46ffc
Marlon <Marlonnlmx@outlook.com>
2022-05-27 09:43:10 -0300

    Update: Implementing conditionals and exceptions

--- src/usecases/update-user-account-usecase.ts
+++ src/usecases/update-user-account-usecase.ts
@@ -1,5 +1,6 @@
 import { User } from "../domain/entity/User";
 import { IUserAccountRepository } from "../repositories/iUserAccountRepository";
+import { AppError } from "../infra/shared/errors/AppError";
 
 interface IUpdateUserAccountUseCaseRequest {
   id: string;
@@ -19,10 +20,27 @@ export class UpdateUserAccountUseCase {
     email,
     password,
   }: IUpdateUserAccountUseCaseRequest): Promise<User> {
-    // const checkUserID = await this.userAccountRepository.findById(id);
+    const checkUserID = await this.userAccountRepository.findById(id);
 
-    // if (!checkUserID) {
-    // }
+    if (checkUserID === null) {
+      throw new AppError("ID does not exist!");
+    }
+
+    const checkUsernameExists = await this.userAccountRepository.findByUsername(
+      username
+    );
+
+    const checkEmailExists = await this.userAccountRepository.findByEmail(
+      email
+    );
+
+    if (checkUsernameExists) {
+      throw new AppError("This username is already in use!");
+    }
+
+    if (checkEmailExists) {
+      throw new AppError("This email is already in use!");
+    }
 
     const updateUser = await this.userAccountRepository.update({
       id,
