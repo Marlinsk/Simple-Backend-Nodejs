410ed3f6bd322ed60be9a66e729d15b7fc8a1061
Marlon <Marlonnlmx@outlook.com>
2022-05-28 22:38:57 -0300

    Update: Removing imports

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import cors from "cors";
-import express, { json } from "express";
-import { routes } from "./infra/http/routes/routes";
+import express from "express";
+
+import { routes } from "./routes";
 
 const app = express();
 const port = 5500;
 
-app.use(json());
 app.use(cors());
 app.use(routes);
 
 app.listen(port, () => {
-  console.log(`Server running in localhost:${port}`);
+  console.log(`Server starting 🚀 localhost:${port}`);
 });
