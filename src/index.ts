81c99495e9f3ad11a99705ab504fb7f4f7c725c5
Marlon <Marlonnlmx@outlook.com>
2022-05-31 10:30:50 -0300

    Update: Implementing json format

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import cors from "cors";
-import express from "express";
+import express, { json } from "express";
 
 import { routes } from "./routes";
 
 const app = express();
 const port = 5500;
 
+app.use(json());
 app.use(cors());
 app.use(routes);
 
