08d88440c427e27e9c013315be6968d84de4ab8a
Marlon <Marlonnlmx@outlook.com>
2022-11-16 08:04:26 -0300

    Implementing middleware for handle errors

---
+++ src/middleware/errors.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express"
+import { AppError } from "src/errors/AppError"
+
+export const errorMiddleware = (
+  error: Error & Partial<AppError>,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const statusCode = error.statusCode ?? 500
+  const message = error.statusCode ? error.message : 'Internal Server Error'
+  return response.status(statusCode).json({ error: { problem: message } })
+}
